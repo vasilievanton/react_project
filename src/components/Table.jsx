d4a0a1accbb0ea108b6d6c8a2d7dd1429c31f03c
vasilievanton <vasilievanton36@gmail.com>
2022-04-27 00:25:45 +0300

    now you can add, edit and remove  note

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,15 +13,50 @@ const Table = (props) => {
     const [data, setData] = useState(props.value);
 
     const [userData, setUserData] = useState(initialValue);
-    const [users, setUsers] = useState([data]);
+    const [users, setUsers] = useState(data);
+    console.log(userData);
+    const [editTableUserData, setEditTableUserData] = useState({
+        isEdit: false,
+        indexUser: null,
+    });
+
+    const handleRemoveClick = (id, index) => {
+        console.log(id);
+        setData(data.filter((user, userIndex) => userIndex !== index));
+        setUsers(users.filter((user, userIndex) => userIndex !== index));
+    };
+    console.log(data);
+    console.log(users);
+
+    const isLillFields = userData.name || userData.age || userData.date || userData.name;
 
     const handleSubmitUser = (e) => {
         e.preventDefault();
-        setUsers((prevState) => [...prevState, userData]);
-        setData((prevState) => [...prevState, userData]);
-        setUserData(initialValue);
+        if (isLillFields) {
+            if (editTableUserData.isEdit) {
+                const editedData = users;
+                editedData.splice(editTableUserData.userIndex, 1, userData);
+                setUsers(editedData);
+                setEditTableUserData({
+                    isEdit: false,
+                    indexUser: null,
+                });
+            } else {
+                setUsers((prevState) => [...prevState, userData]);
+                setData((prevState) => [...prevState, userData]);
+            }
+            setUserData(initialValue);
+        }
     };
+
+    const handleClickClean = () => setUserData(initialValue);
     console.log(users);
+    const handleClickEdit = (data, index) => {
+        console.log(data);
+        setUserData(data);
+        setEditTableUserData({ isEdit: true, userIndex: index });
+    };
+
     return (
         <div>
             <table className="table">
@@ -50,22 +85,26 @@ const Table = (props) => {
                             <td>{person.car}</td>
                             <td>
                                 <div>
-                                    <button>Edit</button>
-                                    <button>Delete</button>
+                                    <button onClick={() => handleClickEdit(person, index)}>Edit</button>
+                                    <button onClick={() => handleRemoveClick(person.id, index)}>Remove</button>
                                 </div>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-            <form onSubmit={handleSubmitUser}>
-                <input placeholder="Name" onChange={(e) => setUserData((prevState) => ({ ...prevState, name: e.target.value, id: Date.now() }))} value={userData.name} />
-                <input placeholder="Age" onChange={(e) => setUserData((prevState) => ({ ...prevState, age: e.target.value, id: Date.now() }))} value={userData.age} />
-                <input placeholder="Date" onChange={(e) => setUserData((prevState) => ({ ...prevState, date: e.target.value, id: Date.now() }))} value={userData.date} />
-                <input placeholder="Car" onChange={(e) => setUserData((prevState) => ({ ...prevState, car: e.target.value, id: Date.now() }))} value={userData.car} />
+            <form onSubmit={handleSubmitUser} onReset={handleClickClean}>
                 <div>
+                    <input placeholder="Name" onChange={(e) => setUserData((prevState) => ({ ...prevState, name: e.target.value, id: Date.now() }))} value={userData.name} />
+                    <input placeholder="Age" onChange={(e) => setUserData((prevState) => ({ ...prevState, age: e.target.value, id: Date.now() }))} value={userData.age} />
+                    <input placeholder="Date" onChange={(e) => setUserData((prevState) => ({ ...prevState, date: e.target.value, id: Date.now() }))} value={userData.date} />
+                    <input placeholder="Car" onChange={(e) => setUserData((prevState) => ({ ...prevState, car: e.target.value, id: Date.now() }))} value={userData.car} />
+                </div>
+                <div className="form__btns">
                     <button type="reset">Clear</button>
-                    <button type="submit">Add</button>
+                    <button disabled={!isLillFields} type="submit">
+                        {editTableUserData.isEdit ? 'Edit' : 'Add'}
+                    </button>
                 </div>
                 {/* <input onChange={(event) => setPerson(event.target.value)} type="text" value={person} /> */}
             </form>
