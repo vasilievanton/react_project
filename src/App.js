4ef9c8e861f94040e044f971c796aa81761ad9a9
vasilievanton <vasilievanton36@gmail.com>
2022-04-26 23:15:51 +0300

    now you can add note

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import AppHeader from './components/AppHeader';
 import Table from './components/Table';
 
-function App() {
-    const appHeader = 'Привет react';
-    const fakeJson = [
-        { name: 'Vanya', age: '28', date: '13.04', car: 'yes' },
-        { name: 'Pasha', age: '32', date: '07.09', car: 'no' },
-        { name: 'Leha', age: '41', date: '27.08', car: 'no' },
-        { name: 'Vitya', age: '13', date: '11.01', car: 'yes' },
-    ];
+const appHeader = 'Привет react';
 
+const data = [
+    { id: 2123534253213, name: 'Vanya', age: '28', date: '13.04', car: 'yes' },
+    { id: 8767564356446, name: 'Pasha', age: '32', date: '07.09', car: 'no' },
+    { id: 5465786745645, name: 'Leha', age: '41', date: '27.08', car: 'no' },
+    { id: 4734878748546, name: 'Vitya', age: '13', date: '11.01', car: 'yes' },
+];
+
+function App() {
     return (
         <div className="App">
             <AppHeader value={appHeader} />
-            <Table value={fakeJson} />
+            <Table value={data} />
         </div>
     );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Table.module.css';
 
+const initialValue = {
+    id: '',
+    name: '',
+    age: '',
+    date: '',
+    car: '',
+};
+
 const Table = (props) => {
+    const [data, setData] = useState(props.value);
+
+    const [userData, setUserData] = useState(initialValue);
+    const [users, setUsers] = useState([data]);
+
+    const handleSubmitUser = (e) => {
+        e.preventDefault();
+        setUsers((prevState) => [...prevState, userData]);
+        setData((prevState) => [...prevState, userData]);
+        setUserData(initialValue);
+    };
+    console.log(users);
     return (
-        <table>
-            <thead>
-                <tr>
-                    <td>#</td>
-                    <td>Name</td>
-                    <td>Age</td>
-                    <td>Date</td>
-                    <td>Car</td>
-                </tr>
-            </thead>
-            <tbody>
-                {props.value.map((person, index) => (
+        <div>
+            <table className="table">
+                <thead>
                     <tr>
-                        <td>{index + 1}</td>
-                        <td>{person.name}</td>
-                        <td>{person.age}</td>
-                        <td>{person.date}</td>
-                        <td>{person.car}</td>
+                        <td>№</td>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Age</td>
+                        <td>Date</td>
+                        <td>Car</td>
+                        <td>Actions</td>
+                        {/* {keysHeader.map((key) => (
+                        <td>{key}</td>
+                    ))} */}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {data.map((person, index) => (
+                        <tr key={person.id}>
+                            <td>{index + 1}</td>
+                            <td>{person.id}</td>
+                            <td>{person.name}</td>
+                            <td>{person.age}</td>
+                            <td>{person.date}</td>
+                            <td>{person.car}</td>
+                            <td>
+                                <div>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <form onSubmit={handleSubmitUser}>
+                <input placeholder="Name" onChange={(e) => setUserData((prevState) => ({ ...prevState, name: e.target.value, id: Date.now() }))} value={userData.name} />
+                <input placeholder="Age" onChange={(e) => setUserData((prevState) => ({ ...prevState, age: e.target.value, id: Date.now() }))} value={userData.age} />
+                <input placeholder="Date" onChange={(e) => setUserData((prevState) => ({ ...prevState, date: e.target.value, id: Date.now() }))} value={userData.date} />
+                <input placeholder="Car" onChange={(e) => setUserData((prevState) => ({ ...prevState, car: e.target.value, id: Date.now() }))} value={userData.car} />
+                <div>
+                    <button type="reset">Clear</button>
+                    <button type="submit">Add</button>
+                </div>
+                {/* <input onChange={(event) => setPerson(event.target.value)} type="text" value={person} /> */}
+            </form>
+        </div>
     );
 };
 
